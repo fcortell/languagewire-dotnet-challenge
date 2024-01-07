4c6e8e08792795f65e1c4ab8a6be8d11309fbd43
Fermin Cortell <Fermin Cortell>
2024-01-07 18:07:43 +0100

    Added commands, validator and API endpoints to manage user's transation balance with Add/Substract/Spend operations

---
+++ src/UserService.Application/TranslationBalance/Commands/SubstractTranslationBalance.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using FluentResults;
+using MediatR;
+using UserService.Application.Common.Errors;
+using UserService.Application.Users.Queries;
+using UserService.Domain.Users;
+
+namespace UserService.Application.TranslationBalance.Commands
+{
+    public class SubstractTranslationBalanceCommand : UpdateTranslationBalanceCommand
+    {
+
+    }
+
+    public abstract class SubstractTranslationBalanceHandler : IRequestHandler<SubstractTranslationBalanceCommand, Result<UserDTO>>
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
+
+        public SubstractTranslationBalanceHandler(IUserRepository userRepository, IMapper mapper)
+        {
+            _userRepository = userRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<Result<UserDTO>> Handle(SubstractTranslationBalanceCommand request, CancellationToken cancellationToken)
+    {
+            var user = await _userRepository.GetByIdAsync(request.UserId);
+            if (user is null)
+        {
+                return Result.Fail<UserDTO>(new EntityNotFoundError(request.UserId));
+            }
+            try { 
+
+            user.TranslationBalance -= request.Amount;
+
+            _userRepository.Update(user);
+            await _userRepository.CommitChangesAsync(cancellationToken);
+            UserDTO dto = _mapper.Map<UserDTO>(user);
+
+            return dto;
+        }
+            catch (Exception ex)
+            {
+                Error error = new Error(ex.Message);
+                return Result.Fail<UserDTO>(error);
+
+            }
+}
+    }
+}
